c6020252247e1c33b4be23f914696965bcf75eee
dendiestev <113887713+dendiestev@users.noreply.github.com>
2022-10-14 18:27:00 +0200

    Add files via upload

--- TP2/DENDIEVEL Estévan S2 TP2.py
+++ TP2/DENDIEVEL Estévan S2 TP2.py
@@ -38,40 +38,78 @@ def inverse(binaire:str) -> str :
 
 ### Q3
 
+
 def addition_binaire(nombre1:str, nombre2:str) -> str :
     '''
     >>> addition_binaire('1001','0001')
     '1010'
     >>> addition_binaire('0001','0001')
     '0010'
+    >>> addition_binaire('1010','0101')
+    '1111'
     '''
     nbplusgrand = 0
     resultat = ''
-    retenue = 0
+    result1 = 0
+    retenue = '0'
     if len(nombre1) < len(nombre2):
         nbplusgrand = len(nombre2)
     else:
         nbplusgrand = len(nombre1)
     i=0
     while i < nbplusgrand:
-        ##if nombre1[i] and nombre2[i] == '1' or nombre1[i] and retenue == '1' or nombre2[i] and retenue == '1':
-        if nombre1[i] == '1' and nombre2[i] == '1':
-            retenue = 1
-            resultat = '0'+resultat
-        elif nombre1[i] == '1' and retenue == '1':
-            retenue = 1
-            resultat = '0'+resultat
-        elif retenue == '1' and nombre2[i] == '1':
-            retenue = 1
-            resultat = '0'+resultat
-        else:
-            result1 = int(nombre1[i])+int(nombre2[i])
-            result1 = str(result1)
-            resultat = result1+resultat
-
-            ###resultat = (nombre1[i]+nombre2[i])+resultat
+        result1 = (nbplusgrand-i)-1
+        ## Si nombre 1 == 1
+        if nombre1[result1] == '1':
+            ## Si nombre 1 == 1 & nombre 2 == 0
+            if nombre2[result1] == '0':
+                ## On vérifie la retenue
+                if retenue == '0':
+                    resultat = '1'+resultat
+                    retenue = '0'
+                else:
+                    resultat = '0'+resultat
+                    retenue = '1'
+            ## Si nombre 1 == 1 & nombre 2 == 1
+            else:
+                ## On vérifie la retenue
+                if retenue == '0':
+                    resultat = '0'+resultat
+                    retenue = '1'
+                else:
+                    resultat = '1'+resultat
+                    retenue = '1'
+        if nombre2[result1] == '1':
+            if nombre1[result1] == '0':
+                if retenue == '0':
+                    resultat = '1'+resultat
+                    retenue = '0'
+                else:
+                    resultat = '0'+resultat
+                    retenue = '1'
+        if nombre2[result1] == '0' and nombre1[result1] == '0':
+            if retenue == '0':
+                resultat = '0'+resultat
+                retenue = '0'
+            else:
+                resultat = '1'+resultat
+                retenue = '0'
         i=i+1
     return(resultat)
+
+### --- Exercice 2 --- ###
+
+## Q1 
+
+def complement_a_deux(nombre:int, nb_bits:int) -> str :
+    '''
+    >>> complement_a_deux(-12, 8)
+    '11110100'
+    >>>> complement_a_deux(7,8)
+    '00000111'
+    '''
+
+
 
 
 
